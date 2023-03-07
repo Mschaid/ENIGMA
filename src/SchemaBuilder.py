22305cd9cfdc922b24e932a87e5709325ba0fc6c
Mike Schaid <michael.schaid@northwestern.edu>
2023-03-06 18:33:06 -0600

    schema builder

---
+++ src/SchemaBuilder.py
@@ -0,0 +1,13 @@
+
+import logging
+import numpy as np
+import os
+import pandas as pd
+import sys
+
+
+class SchemaBuilder:
+    def __init__(self):
+
+    def detect_new_directory(self):
+        self.path = 
