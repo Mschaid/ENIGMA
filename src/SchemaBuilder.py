4849713e306004648123f34b77b490c404efc2d2
Mike_Schaid <68658862+Mschaid@users.noreply.github.com>
2023-03-09 11:13:34 -0600

    collect datafiles func

--- src/SchemaBuilder.py
+++ src/SchemaBuilder.py
@@ -37,12 +37,15 @@ class SchemaBuilder:
         self.logger.info(f"Data directory: {self.data_directory}")
         
         
-    def collect_data_files(self,search_keys:list, *file_regex:str):
+    def collect_data_files(self,search_keys:list, file_types:list):
         
-        for key in search_keys:
+        #searh data_directory for files that match the search_keys
+        data_files = [key for key in search_keys if key in self.data_directory]
+        
         
         # csv, feather, pickle, parquet
-        
+        # for files in data_files:
+        #     if file 
         
         
     def aggregate_data(self, filetype:str, ):
