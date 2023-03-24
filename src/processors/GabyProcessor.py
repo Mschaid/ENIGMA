5cd319b31c4d6123aae636b9c0407a072992da0c
Mike Schaid <michael.schaid@northwestern.edu>
2023-03-24 17:25:06 -0500

    gabyprocessor working and tested.

--- src/processors/GabyProcessor.py
+++ src/processors/GabyProcessor.py
@@ -1,5 +1,7 @@
 import os
+import numpy as np
 import pandas as pd
+
 
 from src.SchemaBuilder import SchemaBuilder
 
@@ -18,17 +20,21 @@ class GabyProcessor:
         return self
 
     def get_data(self):
+        df = pd.read_hdf(self.path)
 
         def rename_trial_columns(df):
             df.columns = [f"trial_{i}" for i in range(len(df.columns))]
             return df
 
         self.data = (
-            pd.read_hdf(self.path)
+            df
             .drop(columns=['timestamps', 'mean', 'err'])
             .pipe(rename_trial_columns)
-            .melt(var_name='trial', value_name='signal')
-            .assign(**self.meta_data)
+            .assign(
+                time=lambda df_: np.linspace(-25, 20, df_.shape[0]),
+                **self.meta_data)
+            .melt(id_vars=['mouse_id', 'day', 'event', 'sensor', 'time'], var_name='trial', value_name='signal')
+
         )
         return self
 
@@ -41,20 +47,19 @@ def process_data(file):
 
 
 def main():
-    path = r'R:\Gaby\MetaData_ForMike'
+    path = input("Enter path to data: ")
     builder = SchemaBuilder(path)
     builder.collect_data_files(search_keys=['D1, D2, DA'], file_types=['h5'])
-    # print(builder.matched_file_types)
+
     print('reading data')
     data = [process_data(file) for file in builder.matched_file_types]
     print('aggregate data')
     aggregated_data = pd.concat(data)
+    save_path = os.path.join(path, 'aggregated_data.h5')
+    aggregated_data.to_hdf(save_path, key='data',
+                           mode='w', complib='zlib', complevel=9)
     print(aggregated_data)
 
 
 if __name__ == '__main__':
-    test_path = r"R:\Gaby\MetaData_ForMike\Day7\142-237_Day7_Avoid_D1_z_score_D1.h5"
-    gp = GabyProcessor(test_path)
-    gp.get_meta_data()
-    gp.get_data()
-    print(gp.data)
+    main()
