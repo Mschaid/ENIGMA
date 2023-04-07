97e6b3bc3d3e8eb42192985ab9a8df61ef7e6f15
Mike Schaid <michael.schaid@northwestern.edu>
2023-04-07 17:17:22 -0500

    hot encode method in PP

--- src/processors/Preprocessor.py
+++ src/processors/Preprocessor.py
@@ -7,7 +7,15 @@ class Preprocessor:
     def __init__(self, path_to_data):
         self.path_to_data = path_to_data
 
-    def one_hot_encode(self, df, *labels):
+    def load_data(self):
+        self.data = pd.read_parquet(self.path_to_data)
+        return self
+
+    def one_hot_encode(self, *labels, df=None,):
+        if df is None:
+            df = self.data
+            print(df)
+
         dataframes_w_dummies = [pd.get_dummies(
             df[label], prefix=label) for label in labels]
         concat_df = pd.concat([df, *dataframes_w_dummies], axis=1)
