022190eef53ddad1e3bdc5bf430145fd73eb168b
Mike Schaid <michael.schaid@northwestern.edu>
2023-04-07 16:47:07 -0500

    fixed preprocessor bug

--- src/processors/Preprocessor.py
+++ src/processors/Preprocessor.py
@@ -5,9 +5,9 @@ import numpy as np
 
 class Preprocessor:
     def __init__(self, path_to_data):
-    self.path_to_data = path_to_data
+        self.path_to_data = path_to_data
 
-    def one_hot_encode(df, *labels):
+    def one_hot_encode(self, df, *labels):
         dataframes_w_dummies = [pd.get_dummies(
             df[label], prefix=label) for label in labels]
         concat_df = pd.concat([df, *dataframes_w_dummies], axis=1)
