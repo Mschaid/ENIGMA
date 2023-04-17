885296f1bfcfe9bdc75421f706f6ef7905bb8bb9
Mike Schaid <michael.schaid@northwestern.edu>
2023-04-17 10:57:07 -0500

    GP test and updated model builder

--- src/model_experiemtation/ModelBuilder.py
+++ src/model_experiemtation/ModelBuilder.py
@@ -20,7 +20,7 @@ PATH_TO_PROCESSED_DATA = input('Enter path to processed data: ')
 
 class ModelBuilder:
 
-    def __init__(self, path_to_processed_data, features=None, target=None):
+    def __init__(self, path_to_processed_data, features, target, model_name, model):
         self.path_to_processed_data = path_to_processed_data
         self.features = features
         self.target = target
@@ -29,6 +29,8 @@ class ModelBuilder:
         self.X_test = None
         self.y_train = None
         self.y_test = None
+        self.model_name = model_name
+        self.model = model
 
     def load_data(self):
         self.processed_data = pd.read_parquet(self.path_to_processed_data)
