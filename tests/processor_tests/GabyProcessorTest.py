03f40dfa671cb87827aa22dd82bfea7a1083b676
Mschaid <michael.schaid@northwestern.edu>
2023-04-17 15:11:06 -0500

    gp unit testing complete

--- tests/processor_tests/GabyProcessorTest.py
+++ tests/processor_tests/GabyProcessorTest.py
@@ -1,30 +1,43 @@
+import pandas as pd
 import unittest
 from src.processors.GabyProcessor import GabyProcessor
 
-TEST_PATH =r'/Users/michaelschaid/GitHub/dopamine_modeling/data/test_data_files/gaby_testing/Day7/142-237_Day7_Avoid_D1_z_score_D1.h5'
+
+TEST_PATH =r'/home/mds8301/gaby_test/Day7/142-237_Day7_Avoid_D1_z_score_D1.h5'
 
 class TestGabyProcessor(unittest.TestCase):
+    """ #Summary
+    Testing for Gaby Processor
+
+    """
     
-    
+    @classmethod
     def setUp(self) -> None:
+        """# Summary
+        
+        initializes gaby processor as class method to be used in all functions
+        
+        """
         self.processor = GabyProcessor(path=TEST_PATH)
+        self.processor.get_meta_data()
+        self.processor.get_data()
         
     def test_get_meta_data(self):
-        self.processor.get_meta_data()
-        self.assertIsInstance(self.processor.meta_data, dict)
-        self.assertIsInstance(self.processor.meta_data['mouse_id'], str)  
-        self.assertEqual(self.processor.meta_data['mouse_id'], '142_237')
-        self.assertIsInstance(self.processor.meta_data['day'], int)
-        self.assertEqual(self.processor.meta_data['day'], 7)
-        self.assertIsInstance(self.processor.meta_data['event'], str)
-        self.assertEqual(self.processor.meta_data['event'], 'avoid')
-        self.assertIsInstance(self.processor.meta_data['sensor'], str)
-        self.assertEqual(self.processor.meta_data['sensor'], 'D1')
-
-   
+        
+        self.assertEqual(self.processor.path, TEST_PATH) #tests path attribute  =  TEST_PATH
+        self.assertIsInstance(self.processor.meta_data, dict) # meta data is dictionary 
+        self.assertIsInstance(self.processor.meta_data['mouse_id'], str)  #moue id is string
+        self.assertEqual(self.processor.meta_data['mouse_id'], '142_237') # mouse id is correct
+        self.assertIsInstance(self.processor.meta_data['day'], int) # day is int
+        self.assertEqual(self.processor.meta_data['day'], 7) # day is correct
+        self.assertIsInstance(self.processor.meta_data['event'], str) # event is string
+        self.assertEqual(self.processor.meta_data['event'], 'avoid') #event is correct
+        self.assertIsInstance(self.processor.meta_data['sensor'], str) # sensor is string
+        self.assertEqual(self.processor.meta_data['sensor'], 'D1') # sensor is corect
 
     def test_get_data(self):
-        pass
+        self.assertIsInstance(self.processor.data, pd.DataFrame)
+        self.assertEqual(len(self.processor.data.columns), 7)
     
 
     
