d47f014dcb15e87f8f375de06eefdab1eaf8c4e9
Mschaid <michael.schaid@northwestern.edu>
2023-04-18 15:29:02 -0500

    preprocessor update

--- src/processors/Preprocessor.py
+++ src/processors/Preprocessor.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import pickle
 import numpy as np
 from sklearn.model_selection import train_test_split
 
@@ -19,14 +20,31 @@ class Preprocessor:
 
     """
 
-    def __init__(self, path_to_data):
+    def __init__(self, 
+                 processor_name = None,
+                 path_to_data = None, 
+                 path_to_processed_data = None, 
+                 features = None,
+                 target = None, 
+                 X_train = None, 
+                 X_test = None, 
+                 y_train = None, 
+                 y_test = None):
         """# Summary
         Instantiates a Preprocessor object.
 
         ### Args:
             path_to_data (_type_): _description_
         """
+        self.processor_name = processor_name
         self.path_to_data = path_to_data
+        self.path_to_processed_data = path_to_processed_data
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.features = features
+        self.target = target
 
     def load_data(self):
         """
@@ -40,7 +58,12 @@ class Preprocessor:
         Returns:
             self
         """
-        self.data = pd.read_parquet(self.path_to_data)
+        if self.path_to_data is not None:
+            self.data = pd.read_parquet(self.path_to_data)
+            
+        if self.path_to_processed_data is not None:
+            self.processed_data = pd.read_parquet(self.path_to_processed_data)
+            
         return self
 
     def one_hot_encode(self, *labels, df=None):
@@ -65,8 +88,67 @@ class Preprocessor:
         self.dummy_data = concat_df.drop(columns=packed_labels)
 
         return self
+    
+    def split_train_test(self, test_size=0.2, random_state=42):
+        """ 
 
-    def save_features(self, path):
+    # Summary 
+    Split the processed data into training and testing sets using the train_test_split method from scikit-learn.
+    
+    ## Args:
+    - test_size (float, optional): the proportion of the data to use for testing, should be between 0 and 1. 
+      Defaults to 0.2 (i.e., 20% of the data is used for testing).
+    - random_state (int, optional): controls the randomness of the data splitting process. 
+      Defaults to 42, which means that the same random seed will be used every time the method is called, 
+      ensuring reproducibility of the results.
+
+    ## Returns:
+    - self: the class instance with attributes X_train, X_test, y_train, and y_test. 
+      X_train and y_train are the training features and target data, while X_test and y_test are the testing 
+      features and target data.
+        """
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+                                                                self.processed_data[self.features], 
+                                                                self.processed_data[self.target], 
+                                                                test_size=test_size, 
+                                                                random_state=random_state)
+        return self
+
+    def split_train_by_query(self, column_name, cutoff):
+        
+        """
+        # Summary
+         Splits the processed data into training and testing sets based on a specified column's cutoff value using 
+         Pandas query method.
+
+        ## Args:
+        - column_name (str): name of the column to split the data on.
+        - cutoff (float): the cutoff value to use to split the data, any value less than or equal to the cutoff 
+        will be in the training set, while any value greater than the cutoff will be in the testing set.
+        
+        ## Returns:
+        - None: updates the class instance with four attributes - X_train, X_test, y_train, and y_test. 
+        X_train and y_train are the training features and target data, respectively, which consist of rows from 
+        the processed data where the value of the specified column is less than or equal to the cutoff. 
+        X_test and y_test are the testing features and target data, respectively, which consist of rows from the 
+        processed data where the value of the specified column is greater than the cutoff.
+        """
+
+        
+        under_cut_off_data = self.processed_data.query(
+            f"{column_name} <= {cutoff}")
+        
+        over_cut_off_data = self.processed_data.query(
+            f"{column_name} > {cutoff}")
+        
+        self.X_train = under_cut_off_data[self.features]
+        self.y_train = under_cut_off_data[self.target]
+        
+        self.X_test = over_cut_off_data[self.features]
+        self.y_test = over_cut_off_data[self.target]
+
+    def save_processed_data(self, path):
         """
         # Summary
         saves the processed data to path.
@@ -75,3 +157,30 @@ class Preprocessor:
             - path (str): saves data to this path.
         """
         self.dummy_data.to_parquet(path)
+        
+#TODO save processor to pickle - make 
+
+'''
+check if dir exists, if not, make new dir
+store object in that dir
+'''        
+    def save_processor(self):
+
+        dir = os.path.dirname(self.path_to_processed_data)
+        
+
+if __name__ == "__main__":
+    
+    FEATURES = ['day', 'time', 'trial',
+                'signal', 'event_cue', 'event_shock',
+                'sensor_D1', 'sensor_D2', 'sensor_DA']
+    TARGET = 'signal'
+    processor = Preprocessor(path_to_processed_data='/home/mds8301/gaby_test/processed_data.parquet.gzp',
+                             features = FEATURES,
+                             target = TARGET)
+    processor.load_data()
+    processor.split_train_test()
+    print(processor.X_train)
+    print(processor.X_test)
+
+    
