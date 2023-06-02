e922cfbab1ca8b2cac49258133be34dac721982b
Mschaid <michael.schaid@northwestern.edu>
2023-06-02 10:39:33 -0500

    finished unittesting on protype

--- tests/src_tests/model_tests/protype_unit_test.py
+++ tests/src_tests/model_tests/protype_unit_test.py
@@ -3,6 +3,6 @@ import pandas as pd
 import tensorflow as tf
 from unittest.mock import patch, MagicMock
 
-from src.model_experimentation.ltsm_model_experimentation import *
+from src.model_experimentation.ltsm_protype_simple import *
 
 
