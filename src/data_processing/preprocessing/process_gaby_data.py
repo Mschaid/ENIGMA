46b9b283c14a951e7ac46de73dd365ffec61ac26
Mschaid <michael.schaid@northwestern.edu>
2023-06-08 11:58:29 -0500

    initial RNN_LSTM

--- src/data_processing/preprocessing/process_gaby_data.py
+++ src/data_processing/preprocessing/process_gaby_data.py
@@ -1,6 +1,6 @@
 import logging
 
-from src.processors.Preprocessor import Preprocessor
+from src.data_processing.processors.Preprocessor import Preprocessor
 
 
 # Set up logging
@@ -11,7 +11,7 @@ logging.basicConfig(filename = '/projects/p31961/dopamine_modeling/results/logs/
 PATH_TO_DATA = r'/projects/p31961/gaby_data/aggregated_data/aggregated_data.parquet.gzp'
 PATH_TO_SAVE = r'/projects/p31961/gaby_data/aggregated_data/data_pipeline'
 gaby_processor = Preprocessor(
-    processor_name = '5_day_training_gaby',
+    processor_name = '5_day_training_gaby_ds25',
     path_to_data = PATH_TO_DATA,
     path_to_save = PATH_TO_SAVE,
     features = ['day', 'time', 'trial','event_cue', 
@@ -29,7 +29,7 @@ def process_and_store_data():
     logging.info('splitting data by query')
     gaby_processor.split_train_by_query('day', 5, processed_data=True)
     logging.info('downsampling train and test datasets')
-    gaby_processor.downsample_train_and_test_datasets(n = 100)
+    gaby_processor.downsample_train_and_test_datasets(n = 25)
     logging.info('saving datasets')
     gaby_processor.save_datasets_to_parquet(save_downsampled = True)
     # gaby_processor.save_data_to_h5()
--- src/modeling/Models/RNN_LSTM.py
+++ src/modeling/Models/RNN_LSTM.py
@@ -4,3 +4,8 @@ import pandas as pd
 import numpy as np
 
 import tensorflow as tf
+from tensorflow.keras import Model
+
+
+class RNNLSTM_simple(Model):
+    pass
