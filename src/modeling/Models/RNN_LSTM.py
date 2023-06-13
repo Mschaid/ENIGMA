d8c51a202ebff68cbb8aef54ec62477211a08ceb
Mschaid <michael.schaid@northwestern.edu>
2023-06-13 16:35:24 -0500

    updated lstm init training prototype

--- src/modeling/Models/RNN_LSTM.py
+++ src/modeling/Models/RNN_LSTM.py
@@ -5,23 +5,25 @@ import numpy as np
 
 import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Dense, LSTM, Lambda
+from tensorflow.keras.layers import (Bidirectional, 
+                                     Dense, 
+                                     LSTM, 
+                                     Lambda 
+                                     )
 
 
 class SimpleLSTM(Model):
-    def __init__(self):
+    def __init__(self, sequence_length, input_dimensions):
         super().__init__()
-        self.lambda_1 = Lambda(lambda x: tf.expand_dims(x, axis=-1), input_shape=[None], name = 'Lambda_1')
-        self.lstm = LSTM(64, input_shape = (None, 1), return_sequences=False, name = 'LSTM')
-        self.dense = Dense(1, activation='relu', name = 'Dense_output')
         
+        self.lstm_input_shape = (sequence_length, input_dimensions)
+        
+        self.lambda_1 = Lambda(lambda x: tf.expand_dims(x, axis=-1), input_shape=[None], name='Lambda_1')
+        self.lstm_1 =LSTM(32, input_shape = self.lstm_input_shape, return_sequences=True, name='LSTM_1')
+
+        self.dense = Dense(1, activation='relu', name='Dense_output')
+
     def call(self, inputs):
         x = self.lambda_1(inputs)
-        x = self.lstm(x)
+        x = self.lstm_1(x)
         return self.dense(x)
-    
-if __name__=='__main__':
-    model = SimpleLSTM()
-    model.build(input_shape=(None, 1))
-    model.compile(optimizer='adam', loss='mse')
-    model.summary()
