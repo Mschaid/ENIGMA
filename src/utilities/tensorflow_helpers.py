0ff9a0eb5dbdec5b96a0ad4777acbcfbc37b6b78
Mschaid <michael.schaid@northwestern.edu>
2023-06-16 16:08:30 -0500

    tensorboard cleanup, tf helpers

---
+++ src/utilities/tensorflow_helpers.py
@@ -0,0 +1,32 @@
+
+
+import os
+import datetime
+import tensorflow as tf
+
+
+def set_tensorboard(model_id, save_directory):
+    """
+    Set up TensorBoard for model training. Returns the TensorBoard callback.
+
+    Parameters
+    ----------
+    save_directory : str
+        The directory to save the TensorBoard logs.
+    model_id : str
+        The ID of the model.
+
+    Returns
+    -------
+    tensorflow.keras.callbacks.TensorBoard
+        The TensorBoard callback.
+    """
+    
+    if not os.path.exists(save_directory): os.makedirs(save_directory)
+    
+    
+    date_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    
+    logs_dir = f"{save_directory}/{model_id}/{date_time}"
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logs_dir, histogram_freq=1)
+    return tensorboard_callback
