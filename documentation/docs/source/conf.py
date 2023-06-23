6d4b3de50fe888029f251aae8c1b0e51da2b7466
Mschaid <michael.schaid@northwestern.edu>
2023-06-23 13:18:00 -0500

    docs update

--- documentation/docs/source/conf.py
+++ documentation/docs/source/conf.py
@@ -1,3 +1,9 @@
+
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath('../../../src'))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -13,15 +19,14 @@ author = 'Mike Schaid'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.todo", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
---
+++ documentation/docs/source/src.rst
@@ -0,0 +1,19 @@
+ENIGMA
+===========
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   src.data_processing
+   src.modeling
+
+Module contents
+---------------
+
+.. automodule:: src
+   :members:
+   :undoc-members:
+   :show-inheritance:
