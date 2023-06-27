2b04ae4a9c9c5f137b668f5696da697554884d9c
Mschaid <michael.schaid@northwestern.edu>
2023-06-27 15:10:49 -0500

    stacked lstm 01

--- tests/src_tests/model_tests/StackedLSTM_test.py
+++ tests/src_tests/model_tests/StackedLSTM_test.py
@@ -1,23 +1,52 @@
 import unittest
 import numpy as np
-from src.modeling.Models.StackedLSTM import StackedLSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Lambda
+from src.models.StackedLSTM import StackedLSTM
+
 
 class TestStackedLSTM(unittest.TestCase):
     def setUp(self):
         self.sequence_length = 10
         self.num_features = 5
-        self.lstm_1_units = 64
-        self.model = StackedLSTM(self.sequence_length, self.num_features, self.lstm_1_units)
+        self.lstm_units = 32
+        self.model = StackedLSTM(self.sequence_length, self.num_features, self.lstm_units)
 
-    def test_input_shape(self):
-        inputs = np.zeros((1, self.sequence_length, self.num_features))
-        output = self.model(inputs)
-        self.assertEqual(output.shape, (1, 1))
+    def test_model_output_shape(self):
+        input_data = np.random.rand(1, self.sequence_length, self.num_features)
+        expected_output_shape = (1, 1)
+        output = self.model(input_data)
+        self.assertEqual(output.shape, expected_output_shape)
 
-    def test_output_range(self):
-        inputs = np.random.rand(1, self.sequence_length, self.num_features)
-        output = self.model(inputs)
-        self.assertTrue(np.all(output >= 0))
+    def test_model_layers(self):
+        expected_layers = [
+            Lambda,
+            LSTM,
+            LSTM,
+            Dense
+        ]
+        layers = [layer.__class__ for layer in self.model.layers]
+        self.assertEqual(layers, expected_layers)
+
+    def test_model_summary(self):
+        expected_summary = 'Model: "StackedLSTM"\n' \
+                           '_________________________________________________________________\n' \
+                           'Layer (type)                 Output Shape              Param #   \n' \
+                           '=================================================================\n' \
+                           'Lambda_1 (Lambda)            (None, None, 5, 1)        0         \n' \
+                           '_________________________________________________________________\n' \
+                           'LSTM_1 (LSTM)                (None, None, 32)          4352      \n' \
+                           '_________________________________________________________________\n' \
+                           'LSTM_2 (LSTM)                (None, 32)                8320      \n' \
+                           '_________________________________________________________________\n' \
+                           'Dense_output (Dense)         (None, 1)                 33        \n' \
+                           '=================================================================\n' \
+                           'Total params: 12,705\n' \
+                           'Trainable params: 12,705\n' \
+                           'Non-trainable params: 0\n' \
+                           '_________________________________________________________________\n'
+        self.assertEqual(self.model.summary(), expected_summary)
+
 
 if __name__ == '__main__':
     unittest.main()
