bb708d12df10f4e02006e8bb4f62f7c57231615d
Mschaid <michael.schaid@northwestern.edu>
2023-06-27 16:11:39 -0500

    update readme and stacked lstm tuning commit

--- src/modeling/experiments/stacked_lstm_tuning.py
+++ src/modeling/experiments/stacked_lstm_tuning.py
@@ -1,8 +1,9 @@
 import os
 import pandas as pd
 import tensorflow as tf
-from src.models.StackedLSTM import StackedLSTM
+
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+from src.models.StackedLSTM import StackedLSTM
 
 dataset_dir = '/projects/p31961/gaby_data/aggregated_data/data_pipeline_downsampled/datasets'
 X_train_path = os.path.join(
@@ -20,61 +21,67 @@ X_test = pd.read_parquet(X_test_path)
 y_train = pd.read_parquet(y_train_path)
 y_test = pd.read_parquet(y_test_path)
 
-X_train = X_train[::1000]
-X_test = X_test[::1000]
-y_train = y_train[::1000]
-y_test = y_test[::1000]
+X_train = X_train[::100]
+X_test = X_test[::100]
+y_train = y_train[::100]
+y_test = y_test[::100]
 
+# define space - parameters to tune
 
-# parameters to tune and define search space
-# Define search space
 space = {
-    'sequence_length': hp.choice('sequence_length', [30, 60, 90, 120, 150, 180]),
+    'sequence_length': hp.choice('sequence_legnth',  range(30, 181, 30)),
+    'units': hp.choice('units', range(32, 129, 32)),
     'learning_rate': hp.loguniform('learning_rate', -5, -1),
     'optimizer': hp.choice('optimizer', ['adam', 'rmsprop', 'sgd'])
 }
-# Define trials object
-trials = Trials()
 
-# define objective function
+trials = Trials()
 
 
 def objective(params):
     sequence_length = params['sequence_length']
+    units = params['units']
     learning_rate = params['learning_rate']
     optimizer = params['optimizer']
 
-    # Define model
-    model = StackedLSTM(sequence_length,
+    # set up model
+    model = StackedLSTM(sequence_length=sequence_length,
                         num_features=X_train.shape[1],
-                        lstm_units=32)
+                        lstm_units=units)
 
-    # Compile model
+    # compile model by optimizer
+
     if optimizer == 'adam':
         optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
     elif optimizer == 'rmsprop':
         optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate)
+
     else:
         optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
+
     model.compile(optimizer=optimizer, loss='mse')
 
     # Train model
-    model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test))
+    model.fit(X_train, y_train, epochs=5)
 
-    # Evaluate model
     loss = model.evaluate(X_test, y_test)
 
-    return {'loss': loss, 'status': STATUS_OK}
+    results = {'loss': loss,
+               'status': STATUS_OK}
+    return results
 
 
 def run_trials():
-    # Run hyperparameter search
-    best = fmin(objective, space, algo=tpe.suggest,
-                max_evals=10, trials=trials)
-    print(best)
-    return best
+    best_trial = fmin(fn=objective,
+                      space=space,
+                      algo=tpe.suggest,
+                      max_evals=10,
+                      trials=trials
+                      )
+    print(best_trial)
+    return best_trial
 
 
 if __name__ == '__main__':
-    # Run hyperparameter search
     run_trials()
