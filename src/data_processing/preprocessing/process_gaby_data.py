210a125848f5f237501384f4e8d8a0d968136763
Mschaid <michael.schaid@northwestern.edu>
2023-07-03 20:55:48 -0500

    gproc update with sex and latent func

--- src/data_processing/preprocessing/process_gaby_data.py
+++ src/data_processing/preprocessing/process_gaby_data.py
@@ -1,6 +1,7 @@
 import logging
 
 from src.data_processing.processors.Preprocessor import Preprocessor
+from src.utilities.gaby_processing_helpers import merge_latency_data, merge_sex_data
 
 
 # Set up logging
@@ -9,13 +10,17 @@ logging.basicConfig(filename='/projects/p31961/dopamine_modeling/results/logs/pr
                     format='[%(asctime)s] %(levelname)s - %(message)s')
 
 PATH_TO_DATA = r'/projects/p31961/gaby_data/aggregated_data/downsampled_aggregated_data.parquet.gzp'
+PATH_TO_LATENCY_DATA = "/projects/p31961/gaby_all_raw_data/AA_Latencies.xlsx"
+PATH_TO_SEX_DATA = "/projects/p31961/gaby_all_raw_data/AA_ListofSex.xlsx"
+
+
 PATH_TO_SAVE = r'/projects/p31961/gaby_data/aggregated_data/data_pipeline_downsampled'
 gaby_processor = Preprocessor(
     processor_name='5_day_training_gaby_downsampled',
     path_to_data=PATH_TO_DATA,
     path_to_save=PATH_TO_SAVE,
     features=['day', 'time', 'trial', 'event_cue',
-              'event_shock', 'sensor_D1', 'sensor_D2',  # ! need escape and avoid
+              'event_shock', 'sensor_D1', 'sensor_D2', 'sex_M',  # ! need escape and avoid
               'sensor_DA'],
     target='signal')
 
@@ -25,15 +30,17 @@ def process_and_store_data():
     logging.info(f'loading data from {gaby_processor.path_to_data}')
     gaby_processor.load_data()
 
+    logging.info('merging latency data')
+    gaby_processor.data = merge_latency_data(
+        gaby_processor.data, PATH_TO_LATENCY_DATA)
+    logging.info('merging sex data')
+    gaby_processor.data = merge_sex_data(gaby_processor.data, PATH_TO_SEX_DATA)
     logging.info('one hot encoding')
-    gaby_processor.one_hot_encode(labels=['event', 'sensor'])
-
+    gaby_processor.one_hot_encode(labels=['event', 'sensor', 'sex'])
     logging.info('splitting data by query')
     gaby_processor.split_train_by_query('day', 5, processed_data=True)
-
     logging.info('saving data')
     gaby_processor.save_datasets_to_parquet(save_downsampled=False)
-
     logging.info('data saved')
 
 
