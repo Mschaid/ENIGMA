dc8a6bf6b475a8062b731af1d221519448ae29cc
Mschaid <michael.schaid@northwestern.edu>
2023-07-04 12:47:03 -0500

    update gaby processing, reprocess and save

--- src/data_processing/preprocessing/process_gaby_data.py
+++ src/data_processing/preprocessing/process_gaby_data.py
@@ -1,11 +1,11 @@
 import logging
 
 from src.data_processing.processors.Preprocessor import Preprocessor
-from src.utilities.gaby_processing_helpers import merge_latency_data, merge_sex_data
+from src.utilities.gaby_processing_helpers import merge_latency_data, merge_sex_data, full_data_feature_extraction
 
 
 # Set up logging
-logging.basicConfig(filename='/projects/p31961/dopamine_modeling/results/logs/processing_logs/process_gaby_data.log',
+logging.basicConfig(filename='/projects/p31961/ENIGMA/results/logs/processing_logs/process_gaby_data.log',
                     level=logging.DEBUG,
                     format='[%(asctime)s] %(levelname)s - %(message)s')
 
@@ -19,8 +19,8 @@ gaby_processor = Preprocessor(
     processor_name='5_day_training_gaby_downsampled',
     path_to_data=PATH_TO_DATA,
     path_to_save=PATH_TO_SAVE,
-    features=['day', 'time', 'trial', 'event_cue',
-              'event_shock', 'sensor_D1', 'sensor_D2', 'sex_M',  # ! need escape and avoid
+    features=['day', 'time', 'trial', 'event_cue', 'event_escape', 'event_avoid',
+              'event_shock', 'sensor_D1', 'sensor_D2', 'sex_M',
               'sensor_DA'],
     target='signal')
 
@@ -34,11 +34,18 @@ def process_and_store_data():
     gaby_processor.data = merge_latency_data(
         gaby_processor.data, PATH_TO_LATENCY_DATA)
     logging.info('merging sex data')
+
     gaby_processor.data = merge_sex_data(gaby_processor.data, PATH_TO_SEX_DATA)
+
+    logging.info('extracting features')
+    gaby_processor.data = full_data_feature_extraction(gaby_processor.data)
+
     logging.info('one hot encoding')
     gaby_processor.one_hot_encode(labels=['event', 'sensor', 'sex'])
+
     logging.info('splitting data by query')
     gaby_processor.split_train_by_query('day', 5, processed_data=True)
+
     logging.info('saving data')
     gaby_processor.save_datasets_to_parquet(save_downsampled=False)
     logging.info('data saved')
@@ -49,3 +56,4 @@ if __name__ == '__main__':
     logging.info('initiating processing and storing processor')
     process_and_store_data()
     logging.info('done processing and storing processor')
+
