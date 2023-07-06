44310f4ea04e80a6a51de34b147a27bf0ca1366b
Mschaid <michael.schaid@northwestern.edu>
2023-07-06 15:02:37 -0500

    bined exp 01

--- jobs/binned_trials_experiemnt_01_job.sh
+++ jobs/binned_trials_experiemnt_01_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:a100:2
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 1:00:00
+#SBATCH -t 48:00:00
 #SBATCH --mem=100G
 source activate enigma
 python /projects/p31961/ENIGMA/src/modeling/experiments/binned_trial_experiment_01.py
