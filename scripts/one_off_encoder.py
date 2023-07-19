2155bbe55db210347916f6e168382352954ac44b
Mschaid <michael.schaid@northwestern.edu>
2023-07-19 17:17:28 -0500

    encoder bug fixe

--- scripts/one_off_encoder.py
+++ scripts/one_off_encoder.py
@@ -6,10 +6,10 @@ DATA_PATH = '/projects/p31961/gaby_data/aggregated_data/data_pipeline_full_datas
 
 def encoder_and_overwright(path, column):
     df = pd.read_parquet(path)
-    df = pd.get_dummies(column)
-    df.to_parquet(path)
-    print(df).head()
+    dummies = pd.get_dummies(data=df, columns=column)
+    dummies.to_parquet(path)
+    print(dummies.columns)
 
 
 if __name__ == "__main__":
-    encoder_and_overwright(DATA_PATH, 'mouse_id')
+    encoder_and_overwright(DATA_PATH, ['mouse_id'])
