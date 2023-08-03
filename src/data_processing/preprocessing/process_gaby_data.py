2ccb9e96fb26cb50a1e515c4a86c9436ede66be2
Mschaid <michael.schaid@northwestern.edu>
2023-08-02 20:30:40 -0500

    refactor var names, add behavior data step

--- src/data_processing/preprocessing/process_gaby_data.py
+++ src/data_processing/preprocessing/process_gaby_data.py
@@ -1,7 +1,11 @@
 import logging
 
 from src.data_processing.processors.Preprocessor import Preprocessor
-from src.utilities.gaby_processing_helpers import merge_latency_data, merge_sex_data, full_data_feature_extraction, assign_cumulative_trials
+from src.utilities.gaby_processing_helpers import (merge_latency_data,
+                                                   merge_sex_data,
+                                                   merge_behavior_data,
+                                                   full_data_feature_extraction,
+                                                   assign_cumulative_trials)
 
 
 # Set up logging
@@ -16,15 +20,17 @@ logging.basicConfig(filename=LOG_FILE_PATH,
 PATH_TO_DATA = r'/projects/p31961/gaby_data/aggregated_data/downsampled_aggregated_data.parquet.gzp'
 PATH_TO_LATENCY_DATA = "/projects/p31961/gaby_all_raw_data/AA_Latencies.xlsx"
 PATH_TO_SEX_DATA = "/projects/p31961/gaby_all_raw_data/AA_ListofSex.xlsx"
+PATH_TO_BEHAVIORAL_DATA = "/projects/p31961/gaby_all_raw_data/BehaviorData_ActiveAvoidance.xlsx"
 
 
 PATH_TO_SAVE = r'/projects/p31961/gaby_data/aggregated_data/data_pipeline_full_dataset'
-gaby_processor = Preprocessor(
+gaby_preprocessor = Preprocessor(
     processor_name='5_day_full_dataset',
     path_to_data=PATH_TO_DATA,
     path_to_save=PATH_TO_SAVE,
     features=['time', 'mouse_id', 'sex_M', 'day', 'trial', 'trial_count', 'learning_phase',
               'event_cue', 'event_escape', 'event_avoid',
+              'action_escape', 'action_avoid',
               'latency', 'event_shock', 'sensor_D1', 'sensor_D2', 'sensor_DA'],
     target='signal'
 )
@@ -32,30 +38,36 @@ gaby_processor = Preprocessor(
 
 def process_and_store_data():
 
-    logging.info(f'loading data from {gaby_processor.path_to_data}')
-    gaby_processor.load_data()
+    logging.info(f'loading data from {gaby_preprocessor.path_to_data}')
+    gaby_preprocessor.load_data()
 
+    logging.info('merging behavioral data')
+    gaby_preprocessor.data = merge_behavior_data(
+        gaby_preprocessor.data, PATH_TO_BEHAVIORAL_DATA)
     logging.info('merging latency data')
-    gaby_processor.data = merge_latency_data(
-        gaby_processor.data, PATH_TO_LATENCY_DATA)
-    logging.info('merging sex data')
+    gaby_preprocessor.data = merge_latency_data(
+        gaby_preprocessor.data, PATH_TO_LATENCY_DATA)
 
-    gaby_processor.data = merge_sex_data(gaby_processor.data, PATH_TO_SEX_DATA)
+    logging.info('merging sex data')
+    gaby_preprocessor.data = merge_sex_data(
+        gaby_preprocessor.data, PATH_TO_SEX_DATA)
 
     logging.info('extracting features')
-    gaby_processor.data = full_data_feature_extraction(gaby_processor.data)
+    gaby_preprocessor.data = full_data_feature_extraction(
+        gaby_preprocessor.data)
 
     logging.info("assigning cumulative trials")
-    gaby_processor.data = assign_cumulative_trials(gaby_processor.data)
+    gaby_preprocessor.data = assign_cumulative_trials(gaby_preprocessor.data)
 
     logging.info('one hot encoding')
-    gaby_processor.one_hot_encode(labels=['event', 'sensor', 'sex'])
+    gaby_preprocessor.one_hot_encode(
+        labels=['event', 'sensor', 'sex', 'action'])
 
     logging.info('splitting data by query')
-    gaby_processor.split_train_by_query('day', 5, processed_data=True)
+    gaby_preprocessor.split_train_by_query('day', 5, processed_data=True)
 
     logging.info('saving data')
-    gaby_processor.save_datasets_to_parquet(save_downsampled=False)
+    gaby_preprocessor.save_datasets_to_parquet(save_downsampled=False)
     logging.info('data saved')
 
 
