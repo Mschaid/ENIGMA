b0797a287fad6ab63eb0c262c19c0e7e87223c96
Mschaid <michael.schaid@northwestern.edu>
2023-09-04 17:00:49 -0500

    set up logger, and doc strings

--- src/utilities/os_helpers.py
+++ src/utilities/os_helpers.py
@@ -1,7 +1,7 @@
 
 import os
 import pandas as pd
-
+import logging
 
 def create_dir(path):
     """# Summary
@@ -80,5 +80,56 @@ def save_dataframes_to_parquet(*dataframes, path_to_save):
             df.to_parquet(path, compression='gzip')
             
 def set_up_directories(*dirs):
+        """
+    Create directories if they do not exist.
+
+    Parameters
+    ----------
+    *dirs : str
+        The names of the directories to create.
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    This function creates directories with the specified names if they do not already exist. If a directory already exists with the same name, no action is taken. The `exist_ok` parameter is set to `True` to prevent an error from being raised if the directory already exists.
+
+    Examples
+    --------
+    >>> set_up_directories('my_dir/raw_data', 'my_dir/logs')
+    """
     for dir in dirs:
         os.makedirs(dir, exist_ok=True)
+
+def set_up_logger(file_path):
+        """
+    Set up a logger that writes to a file.
+
+    Parameters
+    ----------
+    file_path : str
+        The path to the log file.
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    This function sets up a logger that writes log messages to a file at the specified file path. The logger is configured to write messages at the DEBUG level and uses the following format:
+
+    [%(asctime)s] %(levelname)s - %(message)s
+
+    The log file is created if it does not exist, and is truncated if it already exists.
+
+    Examples
+    --------
+    >>> set_up_logger('my_log.txt')
+    """
+
+    logging.basicConfig(filename=file_path,
+                    filemode='w',
+                    level=logging.DEBUG,
+                    format='[%(asctime)s] %(levelname)s - %(message)s')
