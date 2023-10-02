a7372a65c4b93a84e16441e59facd6bf8e333aa3
mschaid <michael.schaid@northwestern.edu>
2023-10-02 16:54:47 -0500

    filescraper filtering functionality updated

--- src/data_processing/processors/FileScraper.py
+++ src/data_processing/processors/FileScraper.py
@@ -13,7 +13,7 @@ from loguru import logger
 
 
 class FileScraper:
-    def __init__(self, data_directory: str = None):
+    def __init__(self, data_directory: str = None) -> None:
 
         if data_directory is not None:
             self._directory = data_directory
@@ -31,7 +31,7 @@ class FileScraper:
     def directory(self, value):
         self._directory = value
 
-    def set_directory(self, directory=None):
+    def set_directory(self, directory: str = None):
         if directory is None:
             self.directory
         self._directory = directory
@@ -47,33 +47,122 @@ class FileScraper:
         self._file_names = value
 
     def fetch_all_file_names(self):
+        """
+        Function to fetch all file names in a directory and store them in a list, can be used to reset the file names stored in the class.
+        """
         all_file_names = []
         for root, dirs, files in os.walk(self.directory):
             for file in files:
                 all_file_names.append(os.path.join(root, file))
         self.file_names = all_file_names
 
-    def filter_files_by_extention(self, *extensions):
-        filtered_files = filter(
-            lambda file: file.endswith(extensions), self.file_names)
-        filtered_files = list(filtered_files)
+    def filter_files_by_extention(self, *extensions: str):
+        """
+        Filters the files in the directory by the specified extensions.
 
+        Parameters
+        ----------
+        *extensions : str
+            The extensions to filter the files by.
+
+        Returns
+        -------
+        None
+        """
+
+        # formats extensions if not already formatted correctly
+        formatted_extensions = [
+            self.format_input(ext) for ext in extensions]
+
+        # filters files by extension in in the stored file names of the directory
+        filtered_files = [file for file in self.file_names if any(
+            ext in file for ext in formatted_extensions)]
+
+        # checks if any files were found with the specified extensions, if not, looged and keeps all files in directory
         try:
             assert len(filtered_files) > 0
+            extensions_found = [ext for ext in formatted_extensions if any(
+                ext in file for file in filtered_files)]
+            extensions_not_found = [
+                ext for ext in formatted_extensions if ext not in extensions_found]
+
+            # loggs the files found
+            logger.info(
+                f"Files filtered by extensions found: {filtered_files}")
+            # what ever extensions are not found are logged
+            if len(extensions_not_found) > 0:
+                logger.info(
+                    f"Files of extension type: {extensions_not_found} not found.")
+            else:
+                pass
+
+        # updates _file_names stored as the filtered_files
             self._file_names = filtered_files
-            logger.info(f"files found: {self._file_names}")
-
+        # if no files are found with the specified extensions, all files in directory still saved and info is logged
         except AssertionError as e:
-            logger.info(f"No files found with the specified extention(s).")
+            logger.info(
+                f"No files found with the specified extention(s), all files in directory returned.")
 
-    def filter_files_by_keywords(self, *keywords):
-        filtered_files = filter(lambda list_: any(
-            keyword in list_ for keyword in keywords), self.file_names)
-        filtered_files = list(filtered_files)
+    def filter_files_by_keywords(self, *keywords: str):
+        """
+        Filters the files in the directory by the specified keywords.
+
+        Parameters
+        ----------
+        *keywords : str
+            The keywords to filter the files by.
+
+        Returns
+        -------
+        None
+        """
+
+        # formats keywords into list for searching
+        formatted_keywords = [self.format_input(
+            word, format_type="keyword") for word in keywords]
+        print(formatted_keywords)
+
+        filtered_files = [file for file in self.file_names if any(
+            word in file for word in formatted_keywords)]
+
         try:
-            assert len(filtered_files) > 0
+            assert (len(filtered_files) > 0)
+
+            keywords_found = [word for word in formatted_keywords if any(
+                word in file for file in filtered_files)]
+
+            keywords_not_found = [
+                word for word in formatted_keywords if word not in keywords_found]
+
+            logger.info(f"Files filtered by keywords found: {filtered_files}")
+            if len(keywords_not_found) > 0:
+                logger.info(
+                    f"Files of keyword type: {keywords_not_found} not found.")
+
             self._file_names = filtered_files
-            logger.info(f"files found: {self._file_names}")
 
         except AssertionError as e:
-            logger.info(f"No files found with the specified extention(s).")
+            logger.info(
+                f"No files found with the specified keyword(s), all files that were searched returned.")
+
+    def format_input(self, input, format_type="extension"):
+        """
+        Formats the input to be used in the filter_files_by_extention() and filter_files_by_keywords() methods.
+        input: str
+        format_type: str, default="extension", options=["extension", "keyword"]
+
+        Returns
+        -------
+        str
+            The formatted input.
+        """
+        formatted_input = (
+            input
+            .strip()
+            .strip(",")
+        )
+
+        if format_type == "extension":
+            return formatted_input if formatted_input.startswith(".") else f".{formatted_input}"
+        elif format_type == "keyword":
+            return formatted_input
