98e67b9322e7f60a6b5c4970301525e5b95872c1
mschaid <michael.schaid@northwestern.edu>
2023-10-06 16:48:18 -0500

    aws tab, fls and kw found outputs, new style

--- src/apps/FileScraperGui/FileScraper_GUI.py
+++ src/apps/FileScraperGui/FileScraper_GUI.py
@@ -7,30 +7,43 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def file_scraper():
     # Initialize variables
     directory = ''
     file_extensions = ''
     keywords = ''
-    results = 'show me'
+    files_found_results = ''
+    filetypes_not_found = ''
+    keywords_not_found = ''
 
     # Initialize S3Manager
-    file_scraper = FileScraper()
 
     if request.method == "POST":
-        DIRECTORY = request.form.get('directory') or ''
-        FILE_EXTENSIONS = request.form.get('file_extensions') or ''
+        DIRECTORY = request.form.get('directory') or None
+        FILE_EXTENSIONS = request.form.get('file_extensions') or None
         KEYWORDS = request.form.get('keywords') or None
 
-        file_scraper.scrape_directoy(directory=DIRECTORY,
-                                     file_extensions=FILE_EXTENSIONS,
+        file_scraper = FileScraper(DIRECTORY)
+        file_scraper.scrape_directoy(file_extensions=FILE_EXTENSIONS,
                                      keywords=KEYWORDS)
-        results = file_scraper.file_names
-        file_scraper.fetch_all_file_names()
 
-        html_content = f"""{results}</p>"""
-        return jsonify({"html_content": html_content})
-    return render_template("index.html")
+        files_found_results = file_scraper.search_results['file_names']
+        filetypes_not_found = file_scraper.search_results['extensions_not_found']
+        keywords_not_found = file_scraper.search_results['keywords_not_found']
+
+        files_found_content = f"""{files_found_results}</p>"""
+        filetypes_not_found_content = f"""{filetypes_not_found}</p>"""
+        keywords_not_found_content = f"""{keywords_not_found}</p>"""
+
+        return jsonify({"files_found_content": files_found_content,
+                        "filetypes_not_found_content": filetypes_not_found_content,
+                        "keywords_not_found_content": keywords_not_found_content})
+    return render_template("file_scraper.html")
+
+
+@app.route('/s3', methods=['GET', 'POST'])
+def s3_manager():
+    return render_template("s3_manager.html")
 
 
 if __name__ == "__main__":
