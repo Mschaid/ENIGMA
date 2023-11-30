58754658ff52de3c7c376437cc31a80d0f020c03
Mschaid <michael.schaid@northwestern.edu>
2023-11-30 14:57:35 -0600

    Refactor df_pipeline function and add save_results function

--- modeling/experiments/xgb_regression_signal_params_only_hyperopt_tune.py
+++ modeling/experiments/xgb_regression_signal_params_only_hyperopt_tune.py
@@ -1,6 +1,7 @@
 import logging
 import hydra
-import os
+from functools import partial
+import yaml
 from pathlib import Path
 import xgboost as xgb
 
@@ -12,15 +13,14 @@ from hyperopt import fmin, tpe, hp, STATUS_OK, Trials, space_eval
 
 from src.data_processing.preprocessing.pandas_preprocessors import *
 from src.data_processing.pipelines.ClassifierPipe import ClassifierPipe
+
 from omegaconf import DictConfig, OmegaConf
-
-
-def df_pipeline(df):
+def df_pipeline(df, query=None):
     '''pandas preprocessing specific to this experiment'''
     drop_columns = ["action", "sex", "trial_count", "trial"]
     df_ = (
         df
-        .query("sensor=='DA' or sensor =='D1'")
+        .query(query)
         .pipe(calculate_max_min_signal)
         .pipe(calculate_percent_avoid)
         .drop(columns=drop_columns)
@@ -50,15 +50,28 @@ def hyperopt_experiment(processor, space, max_evals):
                 algo=tpe.suggest,
                 max_evals=max_evals,
                 trials=trials)
+    best_params = space_eval(space, best)
     logging.info('Hyperopt complete')
     logging.info(f'Best params: {space_eval(space, best)}')
+    results = pd.DataFrame(trials.results)
+    
+    return best_params, results
 
-    # best_trials_path = os.path.
-
+    
+def save_results(best_params, results,  experiment_name, experiment_path):
+    '''Writes experiment parameters to yaml file'''
+    params = {'experiment_name': experiment_name,
+              'best_params': best_params}
+    with open(experiment_path / 'params.yaml', 'w') as file:
+        yaml.dump(params, file)
+    logging.info(f'Parameters written to {experiment_path}/params.yaml')
+    logging.info(f'Writing results to {experiment_path}/hyper_opt_results.parquet')
+    pd.to_parquet(results, experiment_path / 'hyper_opt_results.parquet')
+    return None
 
 @hydra.main(version_base=None,
             config_path="conf",
-            config_name="quest_config")
+            config_name="config")
 def main(cfg: DictConfig) -> None:
     OmegaConf.to_yaml(cfg)
     EXPERIMENT_NAME = cfg.quest_config.experiment_name
@@ -68,10 +81,13 @@ def main(cfg: DictConfig) -> None:
     EXPERIMENT_PATH = Path(cfg.quest_config.experiment_dir)
 
     logging.info(f"Experiment name: {EXPERIMENT_NAME}")
+    
+    queried_df_pipeline = partial(df_pipeline, query=cfg.quest_config.experiment)
+    
 
     PROCESSOR_PIPE = (ClassifierPipe(DATA_PATH)
                       .read_raw_data()
-                      .pandas_pipe(df_pipeline)
+                      .pandas_pipe(queried_df_pipeline)
                       .split_by_ratio(target='ratio_avoid')
                       .transform_data()
                       )
@@ -85,10 +101,14 @@ def main(cfg: DictConfig) -> None:
         "subsample": hp.choice('subsample', np.arange(0, 1, 0.2)),
         "reg_lambda": hp.choice('reg_lambda', np.arange(0, 5, 0.2))
     }
-    hyperopt_experiment(processor=PROCESSOR_PIPE,
+    best_params, results = hyperopt_experiment(processor=PROCESSOR_PIPE,
                         space=SEARCH_SPACE,
                         max_evals=1000)
-
+    
+    save_results(best_params=best_params,
+                 results=results,
+                 experiment_name=EXPERIMENT_NAME,
+                 experiment_path=EXPERIMENT_PATH)
 
 if __name__ == "__main__":
     main()
