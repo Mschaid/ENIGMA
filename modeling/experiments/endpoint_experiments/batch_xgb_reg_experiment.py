58b0bebdd443a8234bdc39b522bbc18c2e7a9070
Mschaid <michael.schaid@northwestern.edu>
2024-01-08 15:18:14 -0600

    Refactor batch experiment setup and execution

--- modeling/experiments/endpoint_experiments/batch_xgb_reg_experiment.py
+++ modeling/experiments/endpoint_experiments/batch_xgb_reg_experiment.py
@@ -1,4 +1,26 @@
+import multiprocessing as mp
 from src.data_processing.model_analyzers.experimenters.batch_experimenters import XGBRegBatchExperimenter
+from dataclasses import dataclass
+from typing import Dict, List
+import logging
+
+@dataclass
+class BatchExperimentMetaData:
+    main_path: str
+    number_of_runs: int
+    experiment_conditions: Dict[str, List[str]]
+
+
+def batch_experiment(exp_data: BatchExperimentMetaData):
+
+    batch_experimenter = XGBRegBatchExperimenter(
+        exp_data.main_path, exp_data)
+
+    experiment_directories = batch_experimenter.get_experiment_directories()
+
+    experimenters = batch_experimenter.set_up_experimenters(
+        experiment_directories)
+    batch_experimenter.run_experiments(experimenters, exp_data.number_of_runs)
 
 
 def main():
@@ -10,13 +32,12 @@ def main():
         "with_out_day": ["mouse_id"]
     }
 
-    bacth_experimenter = XGBRegBatchExperimenter(
-        MAIN_PATH, EXPERIMENMT_CONDITIONS)
+    experiment_data = BatchExperimentMetaData(
+        main_path=MAIN_PATH,
+        number_of_runs=NUMBER_OF_RUNS,
+        experiment_conditions=EXPERIMENMT_CONDITIONS)
 
-    experiment_directories = bacth_experimenter.get_experiment_directories()
-    experimenters = bacth_experimenter.set_up_experiments(
-        experiment_directories)
-    bacth_experimenter.run_experiments(experimenters, NUMBER_OF_RUNS)
+    batch_experiment(experiment_data)
 
 
 if __name__ == '__main__':
--- src/data_processing/model_analyzers/experimenters/batch_experimenters.py
+++ src/data_processing/model_analyzers/experimenters/batch_experimenters.py
@@ -1,11 +1,15 @@
+
+import multiprocessing as mp
 from pathlib import Path
 from abc import ABC, abstractmethod
 
-from typing import List
+
+from typing import Dict, List, NewType
 from src.data_processing.model_analyzers.xgb_analyzers.XGBRegAnalyzer import XGBRegAnalyzer, XGBRegAnalyzerFactory
 from src.data_processing.model_analyzers.xgb_analyzers.XGBRegrResults import XGBRegrResults
 from src.data_processing.model_analyzers.experimenters.experimenters import XGBRegExperimenter, XGBRegExperimenterFactory
 
+ExperimentConditions = NewType('ExperimentCondition', Dict[str, List[str]])
 
 
 class BatchExperimeter(ABC):
@@ -14,7 +18,7 @@ class BatchExperimeter(ABC):
         pass
 
     @abstractmethod
-    def get_experimet_directories(self):
+    def get_experiment_directories(self):
         pass
 
     @abstractmethod
@@ -26,25 +30,35 @@ class BatchExperimeter(ABC):
         pass
 
 
+def custom_multi_processor(func):
+    def wrapper(map_to):
+        with mp.Pool(mp.cpu_count()) as p:
+            p.map(func, map_to)
+            p.close()
+            p.join()
+    return wrapper
+
 
 class XGBRegBatchExperimenter(BatchExperimeter):
-    def __init__(self, main_path, experiment_conditions: Dict[str, str]):
+    def __init__(self, main_path, experiment_conditions: ExperimentConditions):
         self.main_path = Path(main_path)
         self.experiment_conditions = experiment_conditions
         self.xgb_regr_analyzer_factory = XGBRegExperimenterFactory
 
     def get_experiment_directories(self):
-        experiment_direcotires = [d for d in self.main_path.rglob(
+        experiment_directoires = [d for d in self.main_path.rglob(
             '*') if d.is_dir() and not d.name.startswith('.')]
-        return experiment_direcotires
+        return experiment_directoires
 
-    def set_up_experiments(self, directories: List[Path]):
+    # @custom_multi_processor
+    def set_up_experimenters(self, directories: List[Path]):
         experimenters = [self.xgb_regr_analyzer_factory(
             d).create_experimenter()for d in directories]
         return experimenters
 
+    # @custom_multi_processor
     def run_experiments(self, experimenters: List[XGBRegExperimenter], number_of_runs: int):
         for exp in experimenters:
-            for condition_name, cls_to_drop in self.experiment_conditions.items():
+            for condition_name, cls_to_drop in self.experiment_conditions.experiment_conditions.items():
                 exp.run_experiment(number_of_runs, cls_to_drop)
                 exp.save_results(condition_name)
