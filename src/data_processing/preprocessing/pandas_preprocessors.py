f4abfa8a044ed6481070e693d69f1b1c9a685a37
mschaid <michael.schaid@northwestern.edu>
2024-02-07 13:27:57 -0600

    norm processor bug fix

--- src/data_processing/preprocessing/pandas_preprocessors.py
+++ src/data_processing/preprocessing/pandas_preprocessors.py
@@ -175,19 +175,17 @@ def normalize_by_baseline(df, baseline_window="time>-5 & time<-2", time_query="t
 def final_experiment_preprocessor(df: pd.DataFrame, baseline_normalizer: Callable,  query: str = None, cols_to_drop: List = None, ) -> pd.DataFrame:
     '''pandas preprocessing specific to this experiment'''
 
-    constant_features_to_drop = ["mouse_id", "action", "trial", "trial_count"]
-    constant_cols_to_drop = check_for_columns_to_drop(df, constant_features_to_drop)
-    
-    if cols_to_drop is None:
-        cols_to_drop = constant_cols_to_drop
-    else:
-        cols_to_drop = constant_cols_to_drop + cols_to_drop
-    
+    constant_features_to_drop = ["mouse_id",
+                                 "sex'," "action", "trial", "trial_count"]
+    constant_cols_to_drop = check_for_columns_to_drop(
+        df, constant_features_to_drop)
+
     df_ = (
         df
         .query(query)
         .pipe(baseline_normalizer)
         .pipe(calculate_max_min_signal)
+        .drop(columns=constant_cols_to_drop)
         .pipe(calculate_percent_avoid)
         .assign(event=lambda df_: df_.event.replace({"avoid": "cross", "escape": "cross"}))
         .drop(columns=cols_to_drop)
