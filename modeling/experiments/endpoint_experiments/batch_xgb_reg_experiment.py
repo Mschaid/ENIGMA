ecd8816dc0cf54cf9aa0602574ae2a36d8b58e53
mschaid <michael.schaid@northwestern.edu>
2024-02-09 15:24:28 -0600

    batch filter with net

--- modeling/experiments/endpoint_experiments/batch_xgb_reg_experiment.py
+++ modeling/experiments/endpoint_experiments/batch_xgb_reg_experiment.py
@@ -33,7 +33,7 @@ def main():
         "with_day": None,
         "without_day": ["day"]
     }
-    FILTER_KEYS = ['normalzied']
+    FILTER_KEYS = ['net']
 
     experiment_data = BatchExperimentMetaData(
         main_path=MAIN_PATH,
