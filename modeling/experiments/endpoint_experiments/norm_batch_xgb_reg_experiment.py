30beeccab0e338aed66e2383adde3e2fcc4a5967
mschaid <michael.schaid@northwestern.edu>
2024-02-20 15:54:51 -0600

    new batch experimeter for norm experiments

---
+++ modeling/experiments/endpoint_experiments/norm_batch_xgb_reg_experiment.py
@@ -0,0 +1,51 @@
+import multiprocessing as mp
+from src.data_processing.model_analyzers.experimenters.batch_experimenters import BatchExperimeter, XGBRegBatchExperimenter, XGBNormRegBatchExperimenter
+from dataclasses import dataclass
+from typing import Dict, List
+import logging
+
+
+@dataclass
+class BatchExperimentMetaData:
+    main_path: str
+    number_of_runs: int
+    experiment_conditions: Dict[str, List[str]]
+    filter_keys: List[str] = None
+
+
+def batch_experiment(exp_data: BatchExperimentMetaData, experimenter: BatchExperimeter = XGBRegBatchExperimenter):
+    batch_experimenter = experimenter(
+        exp_data.main_path, exp_data)
+
+    experiment_directories = batch_experimenter.get_experiment_directories(
+        exp_data.filter_keys)
+    print(f"directories found: {experiment_directories}")
+
+    experimenters = batch_experimenter.set_up_experimenters(
+        experiment_directories)
+    batch_experimenter.run_experiments(experimenters, exp_data.number_of_runs)
+
+
+def main():
+    print("main")
+    NUMBER_OF_RUNS = 30
+    MAIN_PATH = "/projects/p31961/ENIGMA/results/experiments/endpoint_experiments/xgb_reg_da_w_action_no_day_norm_tune"
+    EXPERIMENMT_CONDITIONS = {
+        # "with_day": None,
+        "without_day": None
+    }
+    # FILTER_KEYS = ['norm_elastic_net']
+
+    experiment_data = BatchExperimentMetaData(
+        main_path=MAIN_PATH,
+        number_of_runs=NUMBER_OF_RUNS,
+        experiment_conditions=EXPERIMENMT_CONDITIONS,
+        filter_keys=None)
+
+    print("running batch experiment")
+    batch_experiment(experiment_data, experimenter=XGBNormRegBatchExperimenter)
+    print("done")
+
+
+if __name__ == '__main__':
+    main()
